f6d9d9090b5b262ed6ea626f6c9b3e2136e64c16
Iain Rodger and Nigel Runnels-Moss <ir+nrm@gower.st>
2019-07-03 17:06:56 +0000

    [WIP] working on pathfinder for multi-node search

--- python3b/graphy.py
+++ python3b/graphy.py
@@ -23,9 +23,27 @@ def next_node(starting_node, graph):
             next_nodes.add(match[0])
     return next_nodes
 
-def pathfinder(starting_node, ending_node, graph):
-    return [['B']]
+def minus(graph, node):
+    return [x for x in graph if node not in x]
 
+def pathfinder(starting_node, ending_node, graph):
+    next_nodes = list(next_node(starting_node, graph))
+    print('nn', next_nodes)
+    if ending_node in next_nodes:
+        next_next_nodes = [next_nodes]
+    else:
+        next_next_nodes = [[node] + list(next_node(node, minus(graph, starting_node))) for node in next_nodes]
+    print('nnn', next_next_nodes)
+    lol = [[starting_node] + x for x in next_next_nodes if ending_node in x]
+    return lol
+
+class TestMinus(unittest.TestCase):
+    def test_removing_a_from_simple_graph_returns_b(self):
+        self.assertEqual([], minus(SIMPLE_GRAPH, 'A'))
+
+    def test_remove_a_from_linear_graph_returns_b_c(self):
+        self.assertEqual([('B', 'C')], minus(LINEAR_GRAPH, 'A'))
+        
 class TestGraphy(unittest.TestCase):
     def test_can_get_from_a_to_b(self):
         self.assertEqual({'B'}, next_node('A', SIMPLE_GRAPH))
@@ -54,9 +72,17 @@ class TestGraphy(unittest.TestCase):
     def test_can_get_from_a_to_b_and_c_in_a_triangular_graph(self):
         self.assertEqual({'A', 'B'}, next_node('C', TRIANGULAR_GRAPH))
 
-    def test_pathfinder_return_set_of_paths(self):
-        self.assertEqual([['B']], pathfinder('A', 'B', SIMPLE_GRAPH))
-
+class TestPathFinder(unittest.TestCase):
+    # def test_pathfinder_for_simple_graph(self):
+    #     self.assertEqual([['A', 'B']], pathfinder('A', 'B', SIMPLE_GRAPH), 'Cannot get from A to B')
+    #     self.assertEqual([['B', 'A']], pathfinder('B', 'A', SIMPLE_GRAPH), 'Cannot get from B to A')
+        
+    def test_pathfinder_for_linear_graph(self):
+        self.assertEqual([['A', 'B']], pathfinder('A', 'B', LINEAR_GRAPH), 'Cannot get from A to B')
+        self.assertEqual([['B', 'C']], pathfinder('B', 'C', LINEAR_GRAPH), 'Cannot get from B to C')
+        self.assertEqual([['A', 'B', 'C']], pathfinder('A', 'C', LINEAR_GRAPH), 'Cannot get from# A to C')
+        
+        
 if __name__ == '__main__':
     unittest.main()
 
