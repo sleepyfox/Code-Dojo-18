64fbadde4bed76b3caacdc39ece1326c475cc9a8
Iain Rodger and Nigel Runnels-Moss <ir+nrm@gower.st>
2019-07-03 15:29:17 +0000

    refactored next_nodes to return a set

--- python3b/graphy.py
+++ python3b/graphy.py
@@ -2,45 +2,52 @@ import unittest
 
 def next_node(starting_node, graph):
     if not graph:
-        return []
+        return set()
     
     list_of_matches = [x for x in graph if x[0] == starting_node or x[1] == starting_node]
 
     if not list_of_matches:
-        return []
+        return set()
 
     # get the other element of the tuple for each match B: (A,B),(B,C) = [A,C]
-    next_node_list = []
+    next_nodes = set()
     for match in list_of_matches:
         if match[0] == starting_node:
-            next_node_list.append(match[1])
+            next_nodes.add(match[1])
         else:
-            next_node_list.append(match[0])
-    return next_node_list
+            next_nodes.add(match[0])
+    return next_nodes
 
 
 class TestGraphy(unittest.TestCase):
     def test_can_get_from_a_to_b(self):
-        self.assertEqual(['B'], next_node('A', [('A', 'B')]))
+        self.assertEqual({'B'}, next_node('A', [('A', 'B')]))
 
     def test_can_get_from_b_to_a(self):
-        self.assertEqual(['A'], next_node('B', [('A', 'B')]))
+        self.assertEqual({'A'}, next_node('B', [('A', 'B')]))
 
     def test_no_next_node_in_a_one_node_graph(self):
-        self.assertEqual([], next_node('A', []))
+        self.assertEqual(set(), next_node('A', []))
 
     def test_no_next_node_if_starting_node_not_in_graph(self):
-        self.assertEqual([], next_node('C', [('A', 'B')]))
+        self.assertEqual(set(), next_node('C', [('A', 'B')]))
 
     def test_can_get_from_a_to_b_in_a_three_node_graph(self):
-        self.assertEqual(['B'], next_node('A', [('A', 'B'), ('B', 'C')]))
+        self.assertEqual({'B'}, next_node('A', [('A', 'B'), ('B', 'C')]))
 
     def test_can_get_from_b_to_c_in_a_three_node_graph(self):
-        self.assertEqual(['A', 'C'], next_node('B', [('A', 'B'), ('B', 'C')]))
+        self.assertEqual({'A', 'C'}, next_node('B', [('A', 'B'), ('B', 'C')]))
 
     def test_can_get_from_a_to_b_and_c_in_a_triangular_graph(self):
-        self.assertEqual(['B', 'C'], next_node('A', [('A', 'B'), ('B', 'C'), ('C', 'A')]))
-        
+        self.assertEqual({'B', 'C'}, next_node('A', [('A', 'B'), ('B', 'C'), ('C', 'A')]))
+
+    def test_can_get_from_a_to_b_and_c_in_a_triangular_graph(self):
+        self.assertEqual({'A', 'C'}, next_node('B', [('A', 'B'), ('B', 'C'), ('C', 'A')]))
+
+    def test_can_get_from_a_to_b_and_c_in_a_triangular_graph(self):
+        self.assertEqual({'A', 'B'}, next_node('C', [('A', 'B'), ('B', 'C'), ('C', 'A')]))
+
+    
 if __name__ == '__main__':
     unittest.main()
 
